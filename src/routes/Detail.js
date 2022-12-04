e5670d11fb17f3c43fbd37f91b2b2d598843f761
AugWoo <103727168+AugWoo@users.noreply.github.com>
2022-12-05 01:34:20 +0900

    update movie detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,46 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
+    setLoading(false);
+    setMovie(json.data.movie);
+    console.log(json.data.movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <h2>
+            {movie.title} ({movie.year})
+          </h2>
+          <h4>{movie.description_full}</h4>
+          <ul>
+            {movie.torrents.map((src) => (
+              <li key={src.hash}>
+                <a href={src}>Download Movie</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
